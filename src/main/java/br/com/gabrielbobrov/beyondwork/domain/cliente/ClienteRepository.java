f28c6ef85d8231ba7def486520737e78c1e7bd6e
Gabriel <bielbobrov@hotmail.com>
2021-03-07 17:46:26 -0300

    BeyondWork

---
+++ src/main/java/br/com/gabrielbobrov/beyondwork/domain/cliente/ClienteRepository.java
@@ -0,0 +1,7 @@
+package br.com.gabrielbobrov.beyondwork.domain.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/br/com/gabrielbobrov/beyondwork/infrastructure/web/controller/ClienteController.java
@@ -0,0 +1,39 @@
+package br.com.gabrielbobrov.beyondwork.infrastructure.web.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.gabrielbobrov.beyondwork.application.serice.ClienteService;
+import br.com.gabrielbobrov.beyondwork.domain.cliente.Cliente;
+import br.com.gabrielbobrov.beyondwork.domain.cliente.ClienteRepository;
+
+@Controller
+@RequestMapping("/cliente")
+public class ClienteController {
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
+	//metodo deve estar na public controller
+	@GetMapping("/cadastro")
+	public String home(@ModelAttribute("cliente") Cliente cliente,Model model) {
+		
+		return "/cliente-cadastro";
+	}
+	@PostMapping("/save")
+	public String cadastro( @ModelAttribute("cliente") @Valid Cliente cliente, Model model) {
+		
+		clienteService.saveCliente(cliente);
+		
+		return "/cliente-cadastro";
+	}
+
+}
