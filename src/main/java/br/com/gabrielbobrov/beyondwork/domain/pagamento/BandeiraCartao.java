5483e4512f6178e36817ce693b77da87d1edfc9c
Gabriel <bielbobrov@hotmail.com>
2021-04-12 20:04:16 -0300

    BeyondWork

---
+++ src/main/java/br/com/gabrielbobrov/beyondwork/domain/pagamento/BandeiraCartao.java
@@ -0,0 +1,5 @@
+package br.com.gabrielbobrov.beyondwork.domain.pagamento;
+
+public enum BandeiraCartao {
+	VISA, MASTER, ELO, AMEX;
+}
--- src/main/java/br/com/gabrielbobrov/beyondwork/domain/pagamento/Pagamento.java
+++ src/main/java/br/com/gabrielbobrov/beyondwork/domain/pagamento/Pagamento.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import br.com.gabrielbobrov.beyondwork.domain.pedido.Pedido;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
